ffd83345dcb878d539e5462ad939092d7c96fcba
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-12-23 18:24:57 +0530

    first commit

---
+++ lib/utils/contants/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF16202A);
+  static const Color secondaryColor = Color(0xFF1F2F3E);
+  static const Color buttonColor = Color(0xFF35CCCD);
+  static const Color underlineColor = Color(0xFF9A9DA1);
+  static const Color textColor = Color(0xFFF5FEF9);
+  static const Color errorColor = Color(0xFFF41F41);
+  static const Color verifiedColor = Color(0xFF75FBB9);
+  static const Color blueColor = Color(0xFF007BFE);
+  static const Color mustardColor = Color(0xFFD0AF10);
+}
---
+++ lib/utils/contants/typography/app_typography.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static TextStyle heading1 = const TextStyle(fontSize: 40, fontWeight: FontWeight.w700, height: 0.03);
+  static TextStyle heading2 = const TextStyle(fontSize: 24, fontWeight: FontWeight.w600, height: 0.05);
+  static TextStyle heading3 = const TextStyle(fontSize: 16, fontWeight: FontWeight.w600, height: 0.09, letterSpacing: 0.64);
+  static TextStyle heading4 = const TextStyle(fontSize: 10, fontWeight: FontWeight.w400, height: 0.12, letterSpacing: 0.40);
+}
